307113ba88a1f521aad699b28b54fccaf0c4ba08
acfcoder <acfcoder@gmail.com>
2023-06-11 10:14:40 +0200

    prueba2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
 import { PokemonComponent } from './components/pokemon/pokemon.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PokemonListComponent,
-    PokemonComponent
+    PokemonComponent,
+   
   ],
   imports: [
+    RouterModule,
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+   PokemonListComponent,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/pokemon-list/pokemon-list.component.ts
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -1,38 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 import { Pokemon } from 'src/app/models/pokemon.interface';
 import { PokemonService } from 'src/app/services/pokemon.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { ActivatedRoute, Router } from '@angular/router';
+import {MatTableModule} from '@angular/material/table';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {NgIf} from '@angular/common';
+
 
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
-  styleUrls: ['./pokemon-list.component.scss']
+  styleUrls: ['./pokemon-list.component.scss'],
+  standalone: true,
+  imports: [
+    MatTableModule,
+    NgIf,
+    MatProgressSpinnerModule,
+    MatTableModule,
+  ],
+
 })
-export class PokemonListComponent {
-
+export class PokemonListComponent implements OnInit {
   pokemons: Pokemon[] = [];
-  pokeData = [];
+  displayedColumns: string[] = ['order', 'name'];
+  dataSource: MatTableDataSource<Pokemon>;
 
-  constructor(private pokemonService: PokemonService) {}
+  constructor(
+    private pokemonService: PokemonService) {
+    this.dataSource = new MatTableDataSource<Pokemon>(this.pokemons);
+  }
 
   ngOnInit(): void {
-
-    for (let i = 1; i < 15; i++){
-      this.pokemonService.getAllPokemons(i).subscribe(
-        pokeData => {
-          const pokemon = {
-            id: pokeData.id,
-            img: pokeData.sprites.other.home.front_default,
-            name: pokeData.name,
-            height: pokeData.height,
-            weight: pokeData.weight,
-            order: pokeData.order,
-            type: pokeData.types,
-            moves: pokeData.moves,
-          };
-          this.pokemons.push(pokemon);
-        }
-      );
+    for (let i = 1; i < 15; i++) {
+      this.pokemonService.getAllPokemons(i).subscribe(pokeData => {
+        const pokemon: Pokemon = {
+          id: pokeData.id,
+          img: pokeData.sprites.other.home.front_default,
+          name: pokeData.name,
+          height: pokeData.height,
+          weight: pokeData.weight,
+          order: pokeData.order,
+          type: pokeData.types,
+          moves: pokeData.moves
+        };
+        this.pokemons.push(pokemon);
+        this.dataSource.data = this.pokemons;
+      });
     }
-   console.log (this.pokemons);
   }
 }
